8041c73d2117a84c0d2af3cf08eeddaaa61191ad
jithin <jithin@embibe.com>
2019-01-20 01:21:34 +0530

    update todo file

--- src/todo.js
+++ src/todo.js
@@ -1,12 +1,16 @@
 /**
  * @todo
  * handle click outside
+ * preselected values(change != to !==)
+ * grouping splitter
+ *
  * animation
  * good naming convensions
  * width
  * reduce lines of code
  *
  * check size of react-select and compare with this
+ * move default props and types
  */
 /**
  * @todo next version --
